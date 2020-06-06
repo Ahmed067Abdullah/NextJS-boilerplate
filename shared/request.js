3b17be83ccf1b4b12a643076657491877404eea8
Ahmed067Abdullah <ahmed@sudofy.com>
2020-06-06 21:59:35 +0500

    fix

--- shared/request.js
+++ shared/request.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_ENDPOINT = 'http://certificate-verifier.herokuapp.com/api';
+const API_ENDPOINT = 'API_URL_HERE';
 
 const fetchToken = () => {
   const token = localStorage.getItem("certificate-verifier-token");
